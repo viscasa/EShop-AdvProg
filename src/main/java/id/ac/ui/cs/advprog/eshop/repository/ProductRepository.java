c973c33267d1364045a7f7d91848faf6b4396f77
Attar <attaralwie@gmail.com>
2025-02-14 00:54:07 +0700

    feat: implement ProductService with basic methods
    - Annotate ProductRepository with @Repository for Spring integration.
    - Add ProductService interface with `create` and `findAll` methods.
    - Implement ProductServiceImpl to handle product creation and retrieval.
    This enables basic product management functionality in the service layer.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
+@Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
