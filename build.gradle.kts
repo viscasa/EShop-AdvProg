b70b09001134cca0ceb153984ebac340e50d1542
Attar <attaralwie@gmail.com>
2025-02-20 21:16:15 +0700

    build: fix sonarcloud CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -79,5 +80,13 @@ tasks.jacocoTestReport {
     reports {
         html.required = true
         xml.required = true
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "viscasa_EShop-AdvProg")
+        property("sonar.organization", "viscasa")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
