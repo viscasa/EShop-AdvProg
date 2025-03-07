5f55cc55bcaedc9c7f9e3cc73c686d3aec555db0
Attar <attaralwie@gmail.com>
2025-03-07 20:20:01 +0700

    [RED] Fix on test repository.

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -1,13 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
-import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,29 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PaymentRepositoryTest {
     PaymentRepository paymentRepository;
-
-    OrderRepository orderRepository;
-    List<Order> orders;
     List<Payment> payments;
 
     @BeforeEach
     void setUp() {
-        orderRepository = new OrderRepository();
-
-        List<Product> products = new ArrayList<>();
-        Product product1 = new Product();
-        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
-        product1.setProductName("Sampo Cap Bambang");
-        product1.setProductQuantity(2);
-        products.add(product1);
-
-        orders = new ArrayList<>();
-        Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b", products, 1708560000L,"Safira Sudrajat");
-        orders.add(order1);
-        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078", products, 1708570000L,  "Safira Sudrajat");
-        orders.add(order2);
-        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e", products, 1708570000L,  "Bambang Sudrajat");
-        orders.add(order3);
+        paymentRepository = new PaymentRepository();
 
         String id = "iniuuid1";
         String method = "Payment by Voucher Code";
