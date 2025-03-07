7223f01f2687367aa636903333c464b77008a8c5
Attar <attaralwie@gmail.com>
2025-03-07 21:01:30 +0700

    [GREEN] Implement PaymentRepository class add map order payment.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,14 +1,18 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
+    private Map<Order,Payment> orderPaymentsMap = new HashMap<>();
 
     public Payment save(Payment payment) {
         int i=0;
@@ -31,5 +35,13 @@ public class PaymentRepository {
             }
         }
         return null;
+    }
+
+    public boolean addOrderPaymentMap(Order order, Payment payment) {
+        if (orderPaymentsMap.containsKey(order)) {
+            return false;
+        }
+        orderPaymentsMap.put(order,payment);
+        return true;
     }
 }
